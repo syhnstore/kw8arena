2b037ba3951d9487e92aaea3da73b6374721a1b2
syhnstore <162292321+syhnstore@users.noreply.github.com>
2026-04-23 04:59:58 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
